a4a5a85a30fda8c1c11b301ae59abe927f536995
Rui Sebastiao <ruisebastiao@gmail.com>
2015-04-17 11:56:46 +0100

    Updated edge detection

--- visionprocessing.cpp
+++ visionprocessing.cpp
@@ -4,12 +4,12 @@
 
 
 
-
+using namespace cv;
 
 VisionProcessing::VisionProcessing(QObject *parent, QZXing *decoder) : QObject(parent)
 {
-    m_thresh=180;
-    m_thresh_inner=180;
+    m_thresh=50;
+    m_thresh_inner=50;
 
     m_processAll=false;
 
@@ -52,30 +52,30 @@ void VisionProcessing::ProcessImage(Mat original)
     QList<QString> m_list;
 
 
-   // try
+    // try
     //{
 
 
-        Mat imagegray,image_edges;
+    Mat imagegray,image_edges;
 
-        cvtColor(original,imagegray,CV_BGR2GRAY);
-        int ratio = 3;
+    cvtColor(original,imagegray,CV_BGR2GRAY);
+    int ratio = 3;
 
-      /*  /// Reduce noise with a kernel 3x3
-        blur( imagegray, imagegray, Size(3,3) );
-        int kernel_size = 3;
-        Canny( imagegray, image_edges, m_thresh, m_thresh*ratio, kernel_size );
-
-*/
-
-         threshold(imagegray,imagegray,m_thresh,255,cv::THRESH_BINARY);
+    /// Reduce noise with a kernel 3x3
+    // blur( imagegray, imagegray, Size(3,3) );
+    int kernel_size = 3;
+    Canny( imagegray, image_edges, m_thresh, m_thresh*ratio, kernel_size );
 
 
-        std::vector<std::vector<cv::Point> > image_contours;
 
-        cv::findContours(imagegray, image_contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+     // threshold(imagegray,imagegray,m_thresh,255,cv::THRESH_BINARY);
 
-/*
+
+    std::vector<std::vector<cv::Point> > image_contours;
+
+    cv::findContours(image_edges, image_contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+
+    /*
         dst = Mat::zeros(Imagegray.size().height,Imagegray.size().width,original.type());
 
         cvtColor(detected_edges,detected_edges,CV_GRAY2BGR);
@@ -83,158 +83,184 @@ void VisionProcessing::ProcessImage(Mat original)
         dst.copyTo( original, detected_edges);
         */
 
-        std::vector<cv::Point> image_contours_approx;
+    std::vector<cv::Point> image_contours_approx;
 
-        for (int i = 0; i < image_contours.size(); i++)
+    for (int i = 0; i < image_contours.size(); i++)
+    {
+        // Approximate contour with accuracy proportional
+        // to the contour perimeter
+        cv::approxPolyDP(Mat(image_contours[i]), image_contours_approx, cv::arcLength(cv::Mat(image_contours[i]), true)*0.02, true);
+        Rect label_boundrect=boundingRect(image_contours_approx);
+        // Skip small or non-convex objects
+        double area=contourArea(image_contours[i]);
+        if (std::fabs(area) <10000 ||
+                std::fabs(area)> 200000 ||
+                label_boundrect.tl().x==0 || label_boundrect.tl().y==0 ||
+                label_boundrect.br().x>=original.size().width ||
+                label_boundrect.br().y>= original.size().height
+                )//|| !cv::isContourConvex(approx))
+            continue;
+
+        if (image_contours_approx.size() == 3)
         {
-            // Approximate contour with accuracy proportional
-            // to the contour perimeter
-            cv::approxPolyDP(cv::Mat(image_contours[i]), image_contours_approx, cv::arcLength(cv::Mat(image_contours[i]), true)*0.02, true);
-            Rect label_boundrect=boundingRect(image_contours_approx);
-            // Skip small or non-convex objects
-            double area=cv::contourArea(image_contours[i]);
-            if (std::fabs(area) <10000 ||
-                    std::fabs(area)> 100000 ||
-                    label_boundrect.tl().x==0 || label_boundrect.tl().y==0 ||
-                    label_boundrect.br().x>=original.size().width ||
-                    label_boundrect.br().y>= original.size().height
-                    )//|| !cv::isContourConvex(approx))
-                continue;
+            //setLabel(dst, "TRI", contours[i]);    // Triangles
+        }
+        else if (image_contours_approx.size() >= 4 && image_contours_approx.size() <= 60)
+        {
+            Scalar color = Scalar( 255, 0, 0);
+            drawContours( original, image_contours, i, color, 2, 8, std::vector<Vec4i>(),0, Point() );
 
-            if (image_contours_approx.size() == 3)
+            Mat labelimage=original(label_boundrect);
+
+
+
+
+
+            Mat barcode_edges,labelroigray;
+
+            cvtColor(labelimage,labelroigray,CV_BGR2GRAY);
+
+            /// Reduce noise with a kernel 3x3
+            //  blur( labelroigray, labelroigray, Size(3,3) );
+            int kernel_size = 3;
+
+            dilate(labelroigray, labelroigray,Mat(), Point(-1, -1), 1, 1, 1);
+            double thr1=100;
+            Canny( labelroigray, barcode_edges, thr1,thr1*2, kernel_size );
+
+            //Mat labelroi;
+            //threshold(labelroigray,labelroigray,m_thresh,255,cv::THRESH_BINARY);
+
+
+
+            std::vector<std::vector<cv::Point> > in_labelcontours;
+            cv::findContours(barcode_edges, in_labelcontours, CV_RETR_CCOMP, CV_CHAIN_APPROX_NONE);
+
+            std::vector<cv::Point> barcode_approx;
+
+            for (int i = 0; i < in_labelcontours.size(); i++)
             {
-                //setLabel(dst, "TRI", contours[i]);    // Triangles
-            }
-            else if (image_contours_approx.size() >= 4 && image_contours_approx.size() <= 60)
-            {
-                Scalar color = Scalar( 255, 0, 0);
-                drawContours( original, image_contours, i, color, 2, 8, std::vector<Vec4i>(),0, Point() );
+                Scalar color_in_label = Scalar( 0, 255, 0);
+                Scalar color_barcode_rect = Scalar( 0, 0, 255);
+                //drawContours( labelimage, in_labelcontours, i, color3, 2, 8, std::vector<Vec4i>(),0, Point() );
+                // Approximate contour with accuracy proportional
+                // to the contour perimeter
+                cv::approxPolyDP(cv::Mat(in_labelcontours[i]), barcode_approx, cv::arcLength(cv::Mat(in_labelcontours[i]), true)*0.02, true);
 
-                Mat labelimage=original(label_boundrect);
+                if(barcode_approx.size()<2) continue;
 
-/*
+                Rect barcode_boundrect=boundingRect(barcode_approx);
 
+                //RotatedRect barcodeRect =minAreaRect(in_labelcontours[i]);
+                double area=contourArea(in_labelcontours[i]);
+                double perimeter = cv::arcLength(in_labelcontours[i],true);
+                if(perimeter<20) continue;
 
+                if(barcode_boundrect.height<20) continue;
+                if(barcode_boundrect.width<20) continue;
+                if(barcode_boundrect.width>300) continue;
 
-                Mat barcode_edges,labelroigray;
-
-                cvtColor(labelimage,labelroigray,CV_BGR2GRAY);
-
-                /// Reduce noise with a kernel 3x3
-              //  blur( labelroigray, labelroigray, Size(3,3) );
-             //   int kernel_size = 3;
-                //dilate(labelroigray, labelroigray,Mat(), Point(-1, -1), 1, 1, 1);
-          //      Canny( labelroigray, barcode_edges, m_thresh, m_thresh*ratio, kernel_size );
-
-                //Mat labelroi;
-                threshold(labelroigray,labelroigray,m_thresh,255,cv::THRESH_BINARY);
-
-
-
-                std::vector<std::vector<cv::Point> > in_labelcontours;
-                cv::findContours(labelroigray, in_labelcontours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
-
-                std::vector<cv::Point> barcode_approx;
-
-                for (int i = 0; i < in_labelcontours.size(); i++)
-                {
-                    Scalar color3 = Scalar( 0, 255, 0);
-
-                    // Approximate contour with accuracy proportional
-                    // to the contour perimeter
-                    cv::approxPolyDP(cv::Mat(in_labelcontours[i]), barcode_approx, cv::arcLength(cv::Mat(in_labelcontours[i]), true)*0.02, true);
-
-                    if(barcode_approx.size()<2) continue;
-
-                    Rect barcode_boundrect=boundingRect(barcode_approx);
-
-
-                    // Skip small or non-convex objects
-                    int gap=15;
-
-                    double perimeter = cv::arcLength(in_labelcontours[i],true);
-                    if ( perimeter<600 || perimeter>2000 ||
-                            barcode_boundrect.height<50
-                            //barcode_boundrect.height>160
-                            //std::fabs(area) <100 ||
-                            //std::fabs(area) >80000
+                /*                          barcode_boundrect.height<0 ||
+                            barcode_boundrect.height>1000 ||
+                            barcode_boundrect.width<0 ||
+                            barcode_boundrect.width>1000 ||
+                            std::fabs(area) <30 ||
+                            std::fabs(area) >10000
                             )
                         continue;
+*/
+                drawContours( labelimage, in_labelcontours, i, color_in_label, 2, 8, std::vector<Vec4i>(),0, Point() );
 
-                    drawContours( labelimage, in_labelcontours, i, color3, 2, 8, std::vector<Vec4i>(),0, Point() );
+                //   rectangle( labelimage, barcode_boundrect.tl(), barcode_boundrect.br(), color_barcode_rect, 2, 8, 0 );
 
+
+                Rect barcode_boundrect_adjusted=Rect(label_boundrect.tl(),Size(barcode_boundrect.size().width*2.5,barcode_boundrect.size().height*2.5));
+
+                barcode_boundrect_adjusted-=barcode_boundrect.tl();
+
+
+
+
+
+                /*
                     Scalar color2 = Scalar( 0, 0, 255);
 
 
 
-                    Rect barcode_boundrect_adjusted=barcode_boundrect+cv::Size(4.8*gap, 2*gap);
-                    barcode_boundrect_adjusted.x-=2.4*gap;
-                    barcode_boundrect_adjusted.y-=gap;
+                    Rect barcode_boundrect_adjusted=barcode_boundrect;//+cv::Size(barcode_boundrect.width, barcode_boundrect.height);
+                    //barcode_boundrect_adjusted.x-=barcode_boundrect.width/2;
+                    //barcode_boundrect_adjusted.y-=barcode_boundrect.height/2;
+ */
 
-                    if(barcode_boundrect_adjusted.x<=0){
-                        barcode_boundrect_adjusted.x=1;
+
+                if(barcode_boundrect_adjusted.x<=0){
+                    barcode_boundrect_adjusted.x=1;
+                }
+
+                if(barcode_boundrect_adjusted.y<=0){
+                    barcode_boundrect_adjusted.y=1;
+                }
+
+                if(barcode_boundrect_adjusted.width+barcode_boundrect_adjusted.x>=original.size().width){
+                    barcode_boundrect_adjusted.width=original.size().width-barcode_boundrect_adjusted.x;
+                }
+
+                if(barcode_boundrect_adjusted.height+barcode_boundrect_adjusted.y>=original.size().height){
+                    barcode_boundrect_adjusted.height=original.size().height-barcode_boundrect_adjusted.y;
+                }
+
+                //   rectangle( original, barcode_boundrect_adjusted.tl(), barcode_boundrect_adjusted.br(), color2, 2, 8, 0 );
+
+
+
+                Mat barcodeimg=original(barcode_boundrect_adjusted);
+
+
+
+
+                if(m_decoder!=0){
+                    QImage roiQimg=cvMat2QImage(barcodeimg);
+
+                    QString tag= m_decoder->decodeImage(roiQimg,-1, -1, false);
+
+                    if(tag!=""){
+                        rectangle( original, barcode_boundrect_adjusted.tl(), barcode_boundrect_adjusted.br(), color_barcode_rect, 2, 8, 0 );
+                        //cvMat2QImage()
+                        CvPoint txtpt(label_boundrect.tl());
+
+                        if(txtpt.y<20)
+                            txtpt.y=label_boundrect.height+20;
+                        else
+                            txtpt.y=label_boundrect.y-20;
+
+                        putText(original, tag.toStdString(),txtpt,
+                                FONT_HERSHEY_COMPLEX_SMALL, 2, cvScalar(0,255,0), 2, CV_AA);
+
+
+                        m_list.append(tag);
+                        break;
+                        //emit BarCodeFound(tag);
+
+
+
+
                     }
 
-                    if(barcode_boundrect_adjusted.y<=0){
-                        barcode_boundrect_adjusted.y=1;
-                    }
-
-                    if(barcode_boundrect_adjusted.width>=label_boundrect.width){
-                        barcode_boundrect_adjusted.width=label_boundrect.width-2;
-                    }
-
-                    if(barcode_boundrect_adjusted.height>=label_boundrect.height){
-                        barcode_boundrect_adjusted.height=label_boundrect.height-2;
-                    }
-
-                    rectangle( labelimage, barcode_boundrect_adjusted.tl(), barcode_boundrect_adjusted.br(), color2, 2, 8, 0 );
-*/
-
-                    //Mat barcodeimg=labelimage(label_boundrect);
-                    // cv::imwrite("e:/temp.jpeg",barcodeimg);
-
-
-
-                    if(m_decoder!=0){
-                        QImage roiQimg=cvMat2QImage(labelimage);
-
-                        QString tag= m_decoder->decodeImage(roiQimg,-1, -1, false);
-                        if(tag!=""){
-
-                            //cvMat2QImage()
-                            CvPoint txtpt(label_boundrect.tl());
-
-                            if(txtpt.y<20)
-                                txtpt.y=label_boundrect.height+20;
-                            else
-                                txtpt.y=label_boundrect.y-20;
-
-                            putText(original, tag.toStdString(),txtpt,
-                                    FONT_HERSHEY_COMPLEX_SMALL, 2, cvScalar(0,255,0), 2, CV_AA);
-
-
-                            m_list.append(tag);
-                            //emit BarCodeFound(tag);
-
-
-
-
-                        }
-                    }
-
-
+                }
 
 
             }
+
         }
+    }
 
-        cvtColor(original,original,CV_BGR2RGB);
+    cvtColor(original,original,CV_BGR2RGB);
 
-        const QImage image=VisionProcessing::cvMat2QImage(original);
+    const QImage image=VisionProcessing::cvMat2QImage(original);
 
-        Q_ASSERT(image.constBits() == original.data);
-        emit ImageReady(image);
-  /*  }
+    Q_ASSERT(image.constBits() == original.data);
+    emit ImageReady(image);
+    /*  }
     catch( cv::Exception& e )
     {
         const char* err_msg = e.what();
